27f90e7b6b53fcc11a8b2b911bdd471429ebeded
kv-2 <str_token@mail.ru>
2019-06-17 23:04:08 +0300

    implement the CORS - Cross-Oregin-Resource-Sharing

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json({}));
 
+//CORS - Cross-Oregin-Resource-Sharing
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authoriuzation');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET')
+    return res.status(200).json({});
+  }
+  next();
+});
+
 // Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);

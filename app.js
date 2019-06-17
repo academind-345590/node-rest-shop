89208d92858e8aefd55dd6b518ab3abc1ae0697d
kv-2 <str_token@mail.ru>
2019-06-17 16:57:47 +0300

    install npm morgan to logger & add morgan to app
    npm install --save morgan

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
 
 // Routes which should handle requests
 app.use('/products', productRoutes);

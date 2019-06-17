444afd22701a05c901291997de1551355906a73b
kv-2 <str_token@mail.ru>
2019-06-17 11:53:44 +0300

    add routes get & post to products

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/');
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Handling GET requests to /products'
+  });
+});
+
+router.post('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Handling POST requests to /products'
+  });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const app = express();
 
-const productRouter = require('./api/routes/products');
+const productRoutes = require('./api/routes/products');
 
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: 'It works!'
-  });
-});
+app.use('/products', productRoutes);
 
 module.exports = app;

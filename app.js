b52ae63ec7d9919eec2edd0fb96239cf1f16c536
kv-2 <str_token@mail.ru>
2019-06-17 11:16:18 +0300

    add products routers

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const productRouter = require('./api/routes/products');
+
 app.use((req, res, next) => {
   res.status(200).json({
     message: 'It works!'

dc8c4eccdbe9101c200666e3df21041406a2b25d
kv-2 <str_token@mail.ru>
2019-06-17 16:37:57 +0300

    add comand  : npm start to nodemon server.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+// Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 

c0888d0872593715d50537d4667103e000021684
kv-2 <str_token@mail.ru>
2019-06-17 17:20:54 +0300

    add error messages

--- app.js
+++ app.js
@@ -11,4 +11,19 @@ app.use(morgan('dev'));
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
 module.exports = app;

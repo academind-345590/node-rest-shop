69d3df14a131cb6c5d643b26174b008bf8b15f30
kv-2 <str_token@mail.ru>
2019-06-19 19:04:34 +0300

    connect products whith mongo_atlas

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product')
 
 router.get('/', (req, res, next) => {
   res.status(200).json({
@@ -8,10 +11,18 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-  const product = {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
-    price: req.body.price,
-  };
+    price: req.body.price
+  });
+  product
+    .save()
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => console.log(err));
+
   res.status(200).json({
     message: 'Handling POST requests to /products',
     createProduct: product,

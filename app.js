5f1dbc1645f6753846dacada58b2de54419e1150
kv-2 <str_token@mail.ru>
2019-06-19 17:22:59 +0300

    implement mongoose
    www.mongodb.com/cloud/atlas

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://rodin:' + process.env.MONGO_ATLAS_PW + '@node-rest-shop-8tn3x.mongodb.net/test?retryWrites=true&w=majority', {
+  useMongoClient: true
+});
 
 
 app.use(morgan('dev'));
